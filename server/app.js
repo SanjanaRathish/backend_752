d31dfeaf882b6bc3ed6a5370608331a6a6ff5281
Sanjana Rathish <rathishsanjana@gmail.com>
2023-11-02 11:39:28 +0530

    front in process

---
+++ server/app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express =require("express");
+const app=express();
+
+const port = 8005;
+
+app.listen(port,()=>{
+    console.log('server is running on port number', {port});
+});
