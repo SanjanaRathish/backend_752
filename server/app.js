bcbde4c073764c70fd77c4ed2e612494c0a5e039
Sanjana Rathish <rathishsanjana@gmail.com>
2023-11-06 21:16:19 +0530

    mongodb started

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express =require("express");
 const app=express();
 const mongoose=require("mongoose");
+require("./db/conn");
 
 const port = 8005;
 
---
+++ server/constant/productdata.js
@@ -0,0 +1,77 @@
+const products=[
+    {
+        id:'product1',
+        url:'./bananaras.png',
+        title:{
+            shortTitle:'BananaRas',
+            descrip:'A delightful banana juice crafted with sugarcane jaggery, delivering the natural sweetness and goodness of banana in every sip. Savor the refreshing beverage year-round, to quench your thirst with its irresistible flavor.'
+        },
+        price: 'Rs. 60.00 '
+    },
+    {
+        id:'product2',
+        url:'./banoffee.png',
+        title:{
+            shortTitle:'Banoffee',
+            descrip:'A heavenly combination of banana slices dipped in honey, crafted to perfection for the ultimate snacking experience. These delectable treats are elevated with the flavors of ginger, cardamom, and more, adding a tantalizing twist to your taste buds.'
+        },
+        price: 'Rs. 40.00'
+    },
+    {
+        id:'product3',
+        url:'./nojomen.png',
+        title:{
+            shortTitle:'NojoMen',
+            descrip:'The yummy noodles made out of an incredibly nutritious blend of banana and millets, completely devoid of maida. Experience a delightful twist with the specia taste-enhancing powder gauranteed to satisfy your cravings for delicious noodles.'
+        },
+        price: 'Rs. 70.00'
+    },
+    {
+        id:'product4',
+        url:'./plantano.png',
+        title:{
+            shortTitle:'Plantano',
+            descrip:'The potassium rich banana powder that adds a healthy and flavorful twist to your culinary creations. Effortlessly mix it into Roti dough, kheer, sweets, milkshakes , and more as a taste and nutrition enhancer.'
+        },
+        price: 'Rs. 50.00'
+    },
+    {
+        id:'product5',
+        url:'./banokie.png',
+        title:{
+            shortTitle:'Banokie',
+            descrip:'A healthy baked delight that takes your tea time to benew excitement. Crafted with a blend of nutrient rich millet, banana powder, and whole wheat. Banokie offers guilt-free indulgence without any maida or added sugar.'
+        },
+        price: 'Rs. 50.00'
+    },
+    {
+        id:'product6',
+        url:'./trekker.png',
+        title:{
+            shortTitle:'Trekker',
+            descrip:'A one of a kind delicacy crafted from sun dried Bananas transformed into delectable banana figs carefully coated with natural honey. With no preservatives or chemicals added, Trekker offers a guilt-free pleasure for all banana enthusiasts to savor.'
+        },
+        price: 'Rs. 20.00'
+    },
+    {
+        id:'product7',
+        url:'./trekker.png',
+        title:{
+            shortTitle:'Banokle',
+            descrip:'Pickle made from the exotic banana flower, meticulously crafted with no added chemical preservatives. Indulge in its rich, unique flavours that make it a culinary masterpiece.'
+        },
+        price: 'Rs. 60.00'
+    },
+    {
+        id:'product8',
+        url:'./trekker.png',
+        title:{
+            shortTitle:'Banastem',
+            descrip:'The Banana Stem Juice, a healthy delight with no added preservatives. Enjoy a refreshing and natural beverage that\'s both nutritious and delicious.Indulge in the all-season refreshment of this enticing beverage.'
+        },
+        price: 'Rs. 80.00'
+    }
+]
+
+
+module.exports = products;
