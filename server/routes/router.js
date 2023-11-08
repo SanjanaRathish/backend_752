c8bbb14db7519b6e40ecd2d8f134712b9eadff62
Sanjana Rathish <rathishsanjana@gmail.com>
2023-11-08 10:16:06 +0530

    postman done

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,5 +3,15 @@ const router = new express.Router();
 const Products = require("../models/productsSchema");
 
 
+//get products data api
+router.get("/getproducts",async(req,res)=>{
+    try{
+        const productsdata = await Products.find();
+        //console.log("console the data "+productsdata);
+        res.status(201).json(productsdata);
+    } catch (error) {
+        console.log("error"+error.message);
+    }
+})
 
 module.exports=router;
